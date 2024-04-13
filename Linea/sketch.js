c94a1a64903284248a4dc1e900cdf26d2461efd7
Eduardo Marges <135554783+How1tzer@users.noreply.github.com>
2024-04-13 00:31:50 +0000

    Punto pendiente

--- Linea/sketch.js
+++ Linea/sketch.js
@@ -1,12 +1,43 @@
-let sprite;
-function preload(){
-  sprite = loadImage('/workspaces/Graficacion-y-multimedia/media/flappy-bird-set (1).png')
-}
 function setup() {
   createCanvas(800, 800);
   background(0);
+  drawAsterisk(400, 400, 200);
 }
 
-function draw() {
-  
+function drawAsterisk(x, y, size) {
+  // longitud de las líneas
+  let halfSize = size / 2;
+  let quarterSize = size / 2;
+
+  // color de las líneas
+  stroke(255);
+
+  //línea vertical 
+  let y1 = y - halfSize;
+  let x1 = x;
+  let m = 0; 
+  let x2 = x;
+  let y2 = y + halfSize;
+  line(x1, y1, x2, y2);
+
+  //línea horizontal 
+  y1 = y;
+  x1 = x - halfSize;
+  m = 0; 
+  x2 = x + halfSize;
+  y2 = y;
+  line(x1, y1, x2, y2);
+
+  // Dibujar líneas perpendiculares 
+  let sqrt2 = sqrt(2);
+  let x3 = x - quarterSize / sqrt2;
+  let y3 = y - quarterSize / sqrt2;
+  let x4 = x + quarterSize / sqrt2;
+  let y4 = y + quarterSize / sqrt2;
+  line(x3, y3, x4, y4);
+  let x5 = x - quarterSize / sqrt2;
+  let y5 = y + quarterSize / sqrt2;
+  let x6 = x + quarterSize / sqrt2;
+  let y6 = y - quarterSize / sqrt2;
+  line(x5, y5, x6, y6);
 }
