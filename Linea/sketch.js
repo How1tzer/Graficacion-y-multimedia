138274989078d2e4a247174bf1d144eee4b47960
Eduardo Marges <135554783+How1tzer@users.noreply.github.com>
2024-04-28 01:56:06 +0000

    circulo

--- Linea/sketch.js
+++ Linea/sketch.js
@@ -1,61 +1,36 @@
 function setup() {
-  createCanvas(800, 800);
-  background(0);
-  drawAsterisk(400, 400, 200);
+  createCanvas(600, 400);
+  background(0); 
+  let xc = width / 2;
+  let yc = height / 2;
+  let r = 150; 
+  drawCircle(xc, yc, r);
 }
 
-function drawAsterisk(x, y, size) {
-  // longitud de las líneas
-  let halfSize = size / 2;
-  let quarterSize = size / 2;
+function drawCircle(xc, yc, r) {
+  stroke(255); 
+  strokeWeight(2); 
+  let x = 0;
+  let y = r;
+  let p = 5 / 4 - r;
+  
+  while (x <= y) {
+    point(xc + x, yc + y);
+    point(xc - x, yc + y);
+    point(xc + x, yc - y);
+    point(xc - x, yc - y);
+    point(xc + y, yc + x);
+    point(xc - y, yc + x);
+    point(xc + y, yc - x);
+    point(xc - y, yc - x);
 
-  // color de las líneas
-  stroke(255);
-
-  // Dibujar línea vertical usando DDA
-  ddaLine(x, y - halfSize, x, y + halfSize);
-
-  // Dibujar línea horizontal usando DDA
-  ddaLine(x - halfSize, y, x + halfSize, y);
-
-  // Dibujar líneas perpendiculares usando DDA
-  let sqrt2 = sqrt(2);
-  let x3 = x - quarterSize / sqrt2;
-  let y3 = y - quarterSize / sqrt2;
-  let x4 = x + quarterSize / sqrt2;
-  let y4 = y + quarterSize / sqrt2;
-  ddaLine(x3, y3, x4, y4);
-  let x5 = x - quarterSize / sqrt2;
-  let y5 = y + quarterSize / sqrt2;
-  let x6 = x + quarterSize / sqrt2;
-  let y6 = y - quarterSize / sqrt2;
-  ddaLine(x5, y5, x6, y6);
-}
-
-function ddaLine(x1, y1, x2, y2) {
-  let dx = x2 - x1;
-  let dy = y2 - y1;
-  let steps;
-  let xIncrement, yIncrement;
-
-  // Determinar el número de pasos basado en la distancia más larga (horizontal o vertical)
-  if (abs(dx) > abs(dy)) {
-    steps = abs(dx);
-  } else {
-    steps = abs(dy);
-  }
-
-  // Calcular incrementos para cada eje
-  xIncrement = dx / steps;
-  yIncrement = dy / steps;
-
-  let x = x1;
-  let y = y1;
-
-  // Dibujar puntos en la línea usando el algoritmo DDA
-  for (let i = 0; i < steps; i++) {
-    point(round(x), round(y));
-    x += xIncrement;
-    y += yIncrement;
+    x++;
+    // Actualiza p
+    if (p < 0) {
+      p += 2 * x + 1;
+    } else {
+      y--;
+      p += 2 * (x - y) + 1;
+    }
   }
 }
